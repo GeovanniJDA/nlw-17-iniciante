6266b2e03e717cabf68788b29014e8f634c16c73
@Geovanni_JDA <geovanniazevedo13@gmail.com>
2024-09-11 17:46:54 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
-    value: "Tomar 3L de água todo dia",
-    checked: false
+    value: 'Tomar 3L de água por dia',
+    checked: false,
 }
 
 let metas = [ meta ]
 
 const cadastrarMeta = async () => {
-    const meta = await input({ message: "Digire a meta:"})
+    const meta = await input({ message: "Digite a meta:"})
 
-    if(meta.length == 0){
-        console.log("A meta não pode ser vazia.")
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia.')
         return
     }
 
-    metas.push({
-        value: meta, checked: false
-    })
+    metas.push(
+        { value: meta, checked: false }
+    )
 }
 
-const start = async() => {
-    
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
+
+}
+
+
+const start = async () => {
     while(true){
+        
         const opcao = await select({
             message: "Menu >",
             choices: [
@@ -40,19 +69,20 @@ const start = async() => {
                 }
             ]
         })
-        switch(opcao){
+
+        switch(opcao) {
             case "cadastrar":
                 await cadastrarMeta()
                 console.log(metas)
-            break
+                break
             case "listar":
-                console.log("vamos listar")
-            break
+                await listarMetas()
+                break
             case "sair":
-                console.log("Até a próxima!")
+                console.log('Até a próxima!')
                 return
         }
     }
 }
 
-start()
+start();
