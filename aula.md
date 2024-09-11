109ed671cbf1921b824e3a2037a4798224c65a7c
@Geovanni_JDA <geovanniazevedo13@gmail.com>
2024-09-11 17:09:59 -0300

    inicia o menu da aplicação

--- aula.md
+++ aula.md
@@ -64,3 +64,13 @@ Apresentar os dados
 # Condicionais
 
 - [x] switch
+
+## Módulos em Node.js:
+
+- [x] Importação de módulos (require, CommonJS)
+- [x] Biblioteca 'inquirer' para criar prompts interativos
+
+
+## Programação assíncrona e Promises:
+
+- [x] Uso de funções assíncronas (async/await)
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const start = () => {
-    let count = 0
+const { select } = require('@inquirer/prompts')
+
+const start = async() => {
+    
     while(true){
-        let opcao = "sair"
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
         switch(opcao){
             case "cadastrar":
                 console.log("vamos cadastrar")
@@ -10,6 +28,7 @@ const start = () => {
                 console.log("vamos listar")
             break
             case "sair":
+                console.log("Até a próxima!")
                 return
         }
     }
